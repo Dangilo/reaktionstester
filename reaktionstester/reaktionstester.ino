70bdec4bc54024487387637c62807a67b0d2f614
Daniel Tamm <danieltamm@gmx.net>
2018-12-10 13:16:31 +0100

    added result

--- reaktionstester/reaktionstester.ino
+++ reaktionstester/reaktionstester.ino
@@ -17,93 +17,123 @@ bool isReady = true;
 bool afterStartSeq = false;
 
 void setup()
-{  
+{	
     Serial.begin(9600);
 
   for(int i = 2; i <=8; i++){
     pinMode(i, OUTPUT);
   }
-  pinMode(BUTTON_IN, INPUT);
-  turnOnAllLEDs(100);
-  turnOffAllLEDs(200);
-  turnOnAllLEDs(100);
-  turnOffAllLEDs(200);
-  turnOnAllLEDs(100);
-  turnOffAllLEDs(200);
+	pinMode(BUTTON_IN, INPUT);
+	turnOnAllLEDs(100);
+	turnOffAllLEDs(200);
+	turnOnAllLEDs(100);
+	turnOffAllLEDs(200);
+	turnOnAllLEDs(100);
+	turnOffAllLEDs(200);
 
 }
 void loop()
 {
-  button_state = digitalRead(BUTTON_IN);
-  if(button_state == HIGH && afterStartSeq){
-    elapsedTime = millis() - startTime;
-    Serial.print( elapsedTime);
-    blink();
-    isReady = true;
-        afterStartSeq = false;
-  }else if(!afterStartSeq && isReady && button_state == HIGH){
-    startSequence();
-  }
+	button_state = digitalRead(BUTTON_IN);
+	if(button_state == HIGH && afterStartSeq){
+		elapsedTime = millis() - startTime;
+		Serial.print( elapsedTime);
+		blink();
+      	showResult(elapsedTime);
+		isReady = true;
+      	afterStartSeq = false;
+	}else if(!afterStartSeq && isReady && button_state == HIGH){
+		startSequence();
+	}
 }
 
 void startSequence(){
   isReady = false;
-  int delay_time = rand()%(max_delay-min_delay + 1) + min_delay;
-  int tillLow = rand()%(max_delay_low - min_delay_low + 1) + min_delay_low;
-  digitalWrite(LED_OUT_1, HIGH);
-  digitalWrite(LED_OUT_7, HIGH);
-  delay(delay_time);
-  digitalWrite(LED_OUT_2, HIGH);
-  digitalWrite(LED_OUT_6, HIGH);
-  delay(delay_time);
-  digitalWrite(LED_OUT_3, HIGH);
-  digitalWrite(LED_OUT_5, HIGH);
-  delay(delay_time);
-  digitalWrite(LED_OUT_4, HIGH);
-  delay(tillLow);
-  turnOffAllLEDs(0);
-  afterStartSeq = true;
+	int delay_time = rand()%(max_delay-min_delay + 1) + min_delay;
+	int tillLow = rand()%(max_delay_low - min_delay_low + 1) + min_delay_low;
+	digitalWrite(LED_OUT_1, HIGH);
+	digitalWrite(LED_OUT_7, HIGH);
+	delay(delay_time);
+	digitalWrite(LED_OUT_2, HIGH);
+	digitalWrite(LED_OUT_6, HIGH);
+	delay(delay_time);
+	digitalWrite(LED_OUT_3, HIGH);
+	digitalWrite(LED_OUT_5, HIGH);
+	delay(delay_time);
+	digitalWrite(LED_OUT_4, HIGH);
+	delay(tillLow);
+	turnOffAllLEDs(0);
+	afterStartSeq = true;
 }
 void turnOnAllLEDs(int possibleDelay){
-  digitalWrite(LED_OUT_1, HIGH);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_2, HIGH);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_3, HIGH);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_4, HIGH);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_5, HIGH);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_6, HIGH);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_7, HIGH);
+	digitalWrite(LED_OUT_1, HIGH);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_2, HIGH);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_3, HIGH);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_4, HIGH);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_5, HIGH);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_6, HIGH);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_7, HIGH);
 }
 void turnOffAllLEDs(int possibleDelay){
-  digitalWrite(LED_OUT_1, LOW);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_2, LOW);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_3, LOW);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_4, LOW);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_5, LOW);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_6, LOW);
-  delay(possibleDelay);
-  digitalWrite(LED_OUT_7, LOW);
+	digitalWrite(LED_OUT_1, LOW);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_2, LOW);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_3, LOW);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_4, LOW);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_5, LOW);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_6, LOW);
+	delay(possibleDelay);
+	digitalWrite(LED_OUT_7, LOW);
 }
 void blink(){
-  turnOnAllLEDs(0);
-  delay(100);
-  turnOffAllLEDs(0);
-  delay(100);
-  turnOnAllLEDs(0);
-  delay(100);
-  turnOffAllLEDs(0);
-  delay(100);
-  turnOnAllLEDs(0);
-  delay(100);
-  turnOffAllLEDs(0);
+	turnOnAllLEDs(0);
+	delay(100);
+	turnOffAllLEDs(0);
+	delay(100);
+	turnOnAllLEDs(0);
+	delay(100);
+	turnOffAllLEDs(0);
+	delay(100);
+	turnOnAllLEDs(0);
+	delay(100);
+	turnOffAllLEDs(0);
 }
+
+void showResult(int result){
+  result = result / 100;
+  if(result >= 50){
+    digitalWrite(LED_OUT_1, HIGH);
+  }
+    if(result >= 100){
+    digitalWrite(LED_OUT_2, HIGH);
+  }
+    if(result >= 150){
+    digitalWrite(LED_OUT_3, HIGH);
+  }
+    if(result >= 200){
+    digitalWrite(LED_OUT_4, HIGH);
+  }
+    if(result >= 250){
+    digitalWrite(LED_OUT_5, HIGH);
+  }
+    if(result >= 300){
+    digitalWrite(LED_OUT_6, HIGH);
+  }
+    if(result >= 350){
+    digitalWrite(LED_OUT_7, HIGH);
+  }
+}
+  
+  
+  
+  
