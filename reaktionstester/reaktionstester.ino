aff6c89b235b0fe7ee5476128934ff4a7cc6b407
Daniel <danieltamm@gmx.net>
2019-01-21 12:22:26 +0100

    bugfixes

--- reaktionstester/reaktionstester.ino
+++ reaktionstester/reaktionstester.ino
@@ -5,11 +5,11 @@ int elapsedTime = 0;
 bool isReady = true;
 bool afterStartSeq =false; 
 
-Reaktion reakt(2, 8, 13, 100, 1000, 1000, 10000);
+Reaktion reakt(2, 8, 13, 100, 1000, 100, 1000);
 
 void setup()
 {  
-Serial.begin(9600);
+//Serial.begin(9600);
 reakt.startUp();
 }
 void loop()
@@ -26,6 +26,8 @@ void loop()
     afterStartSeq = false;
   }else if(!afterStartSeq && isReady && reakt.readButton() == HIGH){
     reakt.startSequence();
+    afterStartSeq = true;
+    startTime = millis();
   }
 }
 
